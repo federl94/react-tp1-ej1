799cf31fc79424013637da1839d6ac83dcbd9f0b
Federico <federuedaleal@hotmail.com>
2023-05-22 01:13:09 -0300

    Ejercicio 1 resuelto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Ejercicio1 from './components/ejercicio1'
+
+function App() {
+  
+  return (
+    <>
+      <Ejercicio1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Ejercicio1.jsx
@@ -0,0 +1,9 @@
+const Ejercicio1 = () => {
+    return (
+        <>
+        <h2>Hello World!</h2>
+        </>
+    );
+};
+
+export default Ejercicio1;
